0c9fc6b0a0e86210ab44555f7c9a804f0fb97413
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-07-06 01:23:56 +0400

    Initial Commit

---
+++ embedded-3d-shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  embedded-3d-shop
+//
+//  Created by Андрей Груненков on 06.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
